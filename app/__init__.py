49bdf18dfa123de6de262128bb185cfa56c147cf
Nikos Makropoulos <nmakro@gmail.com>
2020-07-06 13:43:48 +0300

    Remove comments

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,6 @@ import os
 from flask import Flask
 
 
-# import logging
-# logging.basicConfig()
-# logging.getLogger().setLevel(logging.DEBUG)
-# requests_log = logging.getLogger("requests.packages.urllib3")
-# requests_log.setLevel(logging.DEBUG)
-# requests_log.propagate = True
-# import http.client as http_client
-# http_client.HTTPConnection.debuglevel = 1
-
-colors = ["red", "blue", "green"]
-
-
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
