e2d9aa6320b97c8747c0125a6df2d5f377f51a75
Nikos Makropoulos <nmakro@gmail.com>
2020-07-06 14:48:23 +0300

    Add handling for photo not found

--- app/routes.py
+++ app/routes.py
@@ -28,17 +28,19 @@ def photo():
         try:
             img_url = res.json()["urls"]["small"]
             h = requests.head(img_url)
-            content_type = h.headers.get('content-type')
+            content_type = h.headers.get("content-type")
             if content_type.lower() in ["image/jpg", "image/jpeg", "image/png"]:
                 r = requests.get(img_url)
                 if r.status_code == 200:
                     file_ext = content_type.split("/")[1]
-                    file_name = res.json()["id"]+"."+file_ext
+                    file_name = res.json()["id"] + "." + file_ext
                     save_image(img_name=file_name, img_content=r.content)
                     return render_template("photo.html", filename=file_name)
             else:
                 return render_template("415.html"), 415
         except (KeyError, OSError, ImageSaveError, Exception):
             return render_template("500.html"), 500
+    elif res.status_code == 404:
+        return render_template("404.html"), 404
     else:
         return render_template("external_error.html"), res.status_code
